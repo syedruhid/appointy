12317f9d4970bd9404668f9f1f5b675b178af1fa
syedruhid <55664957+syedruhid@users.noreply.github.com>
2021-10-09 21:50:32 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(home))
+	mux.Post("/", http.HandlerFunc(send))
+	mux.Get("/confirmation", http.HandlerFunc(confirmation))
+
+	log.Println("Listening...")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	render(w, "D:/Go/src/projec/templates/home.html", nil)
+}
+
+func send(w http.ResponseWriter, r *http.Request) {
+	msg := &Message{
+		email:    r.PostFormValue("email"),
+		password: r.PostFormValue("password"),
+	}
+
+	if msg.Validate() == false {
+		render(w, "D:/Go/src/projec/templates/home.html", msg)
+		return
+	}
+	if err := msg.Deliver(); err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "D:/Go/src/projec/templates/confirmation.html", http.StatusSeeOther)
+}
+
+func confirmation(w http.ResponseWriter, r *http.Request) {
+	render(w, "D:/Go/src/projec/templates/confirmation.html", nil)
+}
+
+func render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+	}
+}
